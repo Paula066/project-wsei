b22530a917f5ee2605f9039bc0b4dd3ef2b2576f
Paulina <paulina@mymenu.pl>
2020-10-17 12:56:26 +0200

    First page created

--- src/App.js
+++ src/App.js
@@ -1,14 +1,68 @@
 import React from 'react';
 import './App.scss';
 
+const handleClick = () => {
+    console.log('lala');
+}
 class App extends React.Component {
+
   render() {
     return (
         <div>
-          <h1></h1>
+        <Header />
+        <Box />
         </div>
     );
   }
 }
 
+const Header = () => {
+    return (
+        <div className="header">
+            <div className="hamburger" onClick={handleClick}>
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+            <div className="popup">
+                <ul>
+                    <li>
+                        <a href="#">Zaloguj sie</a>
+                    </li>
+                    <li>
+                        <a href="#">Zarejestruj sie</a>
+                    </li>
+                    <li>
+                        <a href="#">Twoje rezerwacje</a>
+                    </li>
+                    <li>
+                        <a href="#">Kontakt</a>
+                    </li>
+                    <li>
+                        <a href="#">O nas</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+const Box = () => {
+    return (
+        <div className="box">
+            <div className="container flex">
+                <div className="localization">
+                    <h2>Lokalizacja</h2>
+                    <input type="text" />
+                    <button>Szukaj</button>
+                </div>
+                <div className="localization">
+                    <h2>Znajdz restaurcje</h2>
+                    <input type="text" />
+                    <button>Szukaj</button>
+                </div>
+            </div>
+        </div>
+    )
+}
 export default App;
