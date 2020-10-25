0b2c570e7139beca11e39720ab801c280fa58a34
Paulina <paulina@mymenu.pl>
2020-10-25 21:46:48 +0100

    Add rwd to main page

--- src/App.js
+++ src/App.js
@@ -92,7 +92,7 @@ export default class App extends Component {
 
     render() {
         return (
-            <div>
+            <div className="main-box">
                 <Header />
                 <Box />
             </div>
