37e67d624adc486ea4320beb8172a05c574cd9f4
Paulina <paulina@mymenu.pl>
2020-10-25 21:04:20 +0100

    add hamburger

--- src/App.js
+++ src/App.js
@@ -1,69 +1,102 @@
-import React from 'react';
+import React, { Component } from "react";
 import './App.scss';
 
-const handleClick = () => {
-    console.log('lala');
+
+class Header extends Component {
+
+    state = {
+        burgerOpen: false,
+    }
+
+    burgerHandler = () => {
+        console.log("lal");
+        this.setState({
+            burgerOpen: !this.state.burgerOpen
+        });
+    }
+
+    render() {
+
+        const burgerOpen = this.state.burgerOpen;
+        return (
+            <div className="header">
+                <Hamburger onClick={this.burgerHandler} />
+                {burgerOpen && <Menu />}
+                <Menu />
+            </div>
+        )
+    }
 }
-class App extends React.Component {
 
-  render() {
-    return (
-        <div>
-            <Header />
-            <Box />
-        </div>
-    );
-  }
-}
+class Hamburger extends Component {
 
+    render() {
 
-const Header = () => {
-    return (
-        <div className="header">
-            <div className="hamburger" onClick={handleClick}>
+        const onClick = this.props.onClick;
+        return (
+            <div className="hamburger" onClick={onClick}>
                 <span></span>
                 <span></span>
                 <span></span>
             </div>
+        )
+    }
+}
+
+class Menu extends Component {
+    render() {
+        return (
             <div className="popup">
                 <ul>
                     <li>
-                        <a href="#">Zaloguj sie</a>
+                        <a href="/#">Zaloguj sie</a>
                     </li>
                     <li>
-                        <a href="#">Zarejestruj sie</a>
+                        <a href="/#">Zarejestruj sie</a>
                     </li>
                     <li>
-                        <a href="#">Twoje rezerwacje</a>
+                        <a href="/#">Twoje rezerwacje</a>
                     </li>
                     <li>
-                        <a href="#">Kontakt</a>
+                        <a href="/#">Kontakt</a>
                     </li>
                     <li>
-                        <a href="#">O nas</a>
+                        <a href="/#">O nas</a>
                     </li>
                 </ul>
             </div>
-        </div>
-    )
+        )
+    }
 }
 
-const Box = () => {
-    return (
-        <div className="box">
-            <div className="container flex">
-                <div className="localization">
-                    <h2>Lokalizacja</h2>
-                    <input type="text" />
-                    <button>Szukaj</button>
-                </div>
-                <div className="localization">
-                    <h2>Znajdz restaurcje</h2>
-                    <input type="text" />
-                    <button>Szukaj</button>
+class Box extends Component {
+    render() {
+        return (
+            <div className="box">
+                <div className="container flex">
+                    <div className="localization">
+                        <h2>Lokalizacja</h2>
+                        <input type="text"/>
+                        <button>Szukaj</button>
+                    </div>
+                    <div className="localization">
+                        <h2>Znajdz restaurcje</h2>
+                        <input type="text"/>
+                        <button>Szukaj</button>
+                    </div>
                 </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
-export default App;
+export default class App extends Component {
+
+    render() {
+        return (
+            <div>
+                <Header />
+                <Box />
+            </div>
+        );
+    }
+}
